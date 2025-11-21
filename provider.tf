5d733bd019e0ab732733ea2b4ee1fa098c225ae4
imsparsh2001 <sparshjaipuriyar@gmail.com>
2025-11-21 11:46:02 +0530

    Remove default values

--- provider.tf
+++ provider.tf
@@ -13,7 +13,10 @@ terraform {
 
 provider "azurerm" {
   features {
-
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    
   }
   subscription_id = "333359c5-cc92-4749-8db5-60b6741fdb15"
 
--- variables.tf
+++ variables.tf
@@ -46,50 +46,42 @@ variable "vm_name" {
 
 variable "create_public_ip" {
   type        = bool
-  default     = true
   description = "Whether to create a public IP for the VM"
 }
 
 variable "public_ip_allocation" {
   type        = string
-  default     = "Static"
   description = "Allocation method for public IP "
 }
 
 variable "admin_username" {
   type        = string
-  default     = "azureuser"
   description = "username for the virtual machine"
 }
 
 variable "admin_ssh_public_key" {
   type        = string
-  default     = ""
   description = "SSH public key for Linux VM authentication"
   sensitive   = false
 }
 
 variable "vm_size" {
   type        = string
-  default     = "Standard_B1s"
   description = "Size of the virtual machine"
 }
 
 variable "os_disk_size_gb" {
   type        = number
-  default     = 30
   description = "Size of the OS disk in GB"
 }
 
 variable "os_disk_type" {
   type        = string
-  default     = "Standard_LRS"
   description = "Storage account type for OS disk "
 }
 
 variable "vm_zone" {
   type        = number
-  default     = null
   description = "Availability zone for the VM"
 }
 
